dca7473e6f3011004bf5f0c80df4921fe8f38db4
Bryson Dunn <k.bryson.dunn@gmail.com>
2015-01-02 17:31:05 -0700

    Enum Generator Upgrade
    - Now updates FontAwesome.java in place
    - Detailed output of icons scraped and replaced.

--- src/com/kbdunn/vaadin/addons/fontawesome/util/GenerateEnum.java
+++ src/com/kbdunn/vaadin/addons/fontawesome/util/GenerateEnum.java
@@ -1,7 +1,9 @@
 package com.kbdunn.vaadin.addons.fontawesome.util;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedHashMap;
@@ -11,55 +13,102 @@ import java.util.Map.Entry;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class GenerateEnum {
 
+	private static final String SOURCE = "http://fortawesome.github.io/Font-Awesome/cheatsheet/";
+	private static final String ENUM_FILE = "C:\\Users\\kdunn\\Documents\\STS\\FontAwesome-Vaadin\\src\\com\\kbdunn\\vaadin\\addons\\fontawesome\\FontAwesome.java";
+	private static final String ENUM_ENTRY_REGEX = "\\s+[A-Z0-9_]+\\(\\\"fa-.+\\)[,;]";
+	
 	public static void main(String[] args) {
+			writeFile(getIcons());
+			System.out.println("Done.");
+	}
+	
+	private static Map<String, String> getIcons() {
 		Map<String, String> icons = new LinkedHashMap<String, String>();
 		try {
-			Document html = Jsoup.connect("http://fortawesome.github.io/Font-Awesome/cheatsheet/").get();
-			Elements cheatsheet = html.getElementsByClass("row"); // Cheatsheet has only 1 "row"
+			Document html = Jsoup.connect(SOURCE).get();
+			String desc = html.getElementsByClass("jumbotron").get(0).getElementsByTag("p").get(0).ownText();
+			System.out.println("Scraping icon data from '" + desc + "' [" + SOURCE + "]....");
 			
 			// Loop through icons, get class and hex, add to map
 			String cssClass, hex;
-			for (Element e : cheatsheet.get(0).getElementsByTag("div")) {
+			for (Element e : html.getElementsByClass("row").get(0).children()) { // Cheatsheet has only 1 .row element
 				cssClass = e.ownText();
 				hex = e.getElementsByClass("muted").get(0).ownText().replace("[&#x", "").replace(";]", "");
-				System.out.println(cssClass + " [" + hex.toString() + "]");
 				icons.put(cssClass, hex);
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("\tRead " + icons.size() + " icons.");
+		return icons;
+	}
+	
+	private static void writeFile(Map<String, String> icons) {
+		System.out.println("Updating " + ENUM_FILE + "....");
+		File infile = new File(ENUM_FILE);
+		File outfile = new File(ENUM_FILE + ".tmp");
+		BufferedReader reader = null;
+		BufferedWriter writer = null;
+		try {
+			reader = new BufferedReader(new FileReader(infile));
+			writer = new BufferedWriter(new FileWriter(outfile));
+			String inline = null;
+			int current = 0;
+			int enumstart = 0;
+			while ((inline = reader.readLine()) != null) {
+				current++;
+				if (enumstart > 0) {
+					// Check if we're still in the enum
+					if (!inline.matches(ENUM_ENTRY_REGEX)) {
+						System.out.println("\tEnum entries end on line " + (current - 1) + ". "
+								+ "Replaced " + (current - enumstart) + " icon entries.");
+						enumstart = 0;
+						writer.write(inline + "\n");
+					}
+				} else if (inline.contains("public enum FontAwesome")) {
+					// Enum entries start on next line
+					enumstart = current + 1;
+					System.out.println("\tEnum entries start on line " + enumstart + ".");
+					writer.write(inline + "\n");
+					
+					// Write the new enum entries
+					writeEnumLines(writer, icons);
+				} else {
+					// Copy the rest of the file
+					writer.write(inline + "\n");
+				}
+			}
 			
-			writeFile(icons);
+			writer.close();
+			reader.close();
+			
+			// Delete FontAwesome.java
+			if (!infile.delete()) 
+				throw new IOException("Could not delete " + infile);
+			
+			// Rename .tmp file
+			if (!outfile.renameTo(infile)) 
+				throw new IOException("Could not rename " + outfile + " to " + infile);
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	private static void writeFile(Map<String, String> icons) {
-		File outfile = new File(ClassLoader.getSystemClassLoader().getResource(".").getPath() + "/enum.txt");
-		BufferedWriter writer = null;
-		try {
-			writer = new BufferedWriter(new FileWriter(outfile));
-			String line;
-			for (Entry<String, String> icon : icons.entrySet()) {
-				line = icon.getKey().toUpperCase().replace("FA-",  "").replace("-", "_");
-				line += "(\"" + icon.getKey() + "\", ";
-				line += "0x" + icon.getValue();
-				line += ")," + String.format("%n");
-				
-				writer.write(line);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (writer != null) writer.close();
-			} catch (Exception e) {
-			    //Ignore
-			}
-	        }
+	private static void writeEnumLines(BufferedWriter writer, Map<String, String> icons) throws IOException {
+		String outline = null;
+		int outlines = 0;
+		int count = icons.size();
+		for (Entry<String, String> icon : icons.entrySet()) {
+			outline = "\t" + icon.getKey().toUpperCase().replace("FA-",  "").replace("-", "_");
+			outline += "(\"" + icon.getKey() + "\", ";
+			outline += "0x" + icon.getValue();
+			outline += ")" + (++outlines == count ? ";" : ",") + "\n";
+			
+			writer.write(outline);
+		}
 	}
 }
